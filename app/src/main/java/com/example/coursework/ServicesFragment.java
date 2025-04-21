5792899df60621a26c0b7c0a86cb6da6d0603945
Anastacyy <bodnar.anastasiia.2007@gmail.com>
2025-04-21 18:16:10 +0300

    a

--- app/src/main/java/com/example/coursework/ServicesFragment.java
+++ app/src/main/java/com/example/coursework/ServicesFragment.java
@@ -1,9 +1,7 @@
 package com.example.coursework;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +11,41 @@ public class ServicesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_services, container, false);
+        View view = inflater.inflate(R.layout.fragment_services, container, false);
 
+        view.findViewById(R.id.haircuts_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, new HairFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        view.findViewById(R.id.makeup_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, new MakeUPFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        view.findViewById(R.id.manicure_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, new NailFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+        return view;
     }
 }
-
